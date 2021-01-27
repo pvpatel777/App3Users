4af38713d8e9617331577aaabbee82732c502144
pvpatel777 <pvpatel_777@yahoo.co.in>
2021-01-27 12:20:57 +0100

    First commit

---
+++ src/main/java/test/my/app3Users/App3Users.java
@@ -0,0 +1,13 @@
+package test.my.app3Users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App3Users {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App3Users.class, args);
+	}
+
+}
---
+++ src/main/java/test/my/app3Users/UserRepository.java
@@ -0,0 +1,9 @@
+package test.my.app3Users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
